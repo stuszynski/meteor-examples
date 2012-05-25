634c5a99d188c628cda8d0c6bdcb442531c071d1
Wlodek Bzyl <matwb@univ.gda.pl>
2012-05-25 19:11:06 +0200

    done with some changes to the leaderboard app

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -3,9 +3,15 @@
 
 Players = new Meteor.Collection("players");
 
+Session.set("score", -1);
+Session.set("name", 1);
+
+
 if (Meteor.is_client) {
+  // Template variables
   Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
+    return Players.find({},
+        {sort: {score: Session.get("score"), name: Session.get("name")}});
   };
 
   Template.leaderboard.selected_name = function () {
@@ -14,12 +20,40 @@ if (Meteor.is_client) {
   };
 
   Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
+    return Session.equals("selected_player", this._id) ? " selected" : '';
   };
+
+  // Events
 
   Template.leaderboard.events = {
     'click input.inc': function () {
       Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    },
+    // reset all scores
+    'click input.reset': function () {
+      Players.find().forEach(function(player) {
+          Players.update(player, {$set: {score: randomScore()}});
+      });
+    },
+    // add and remove scientist
+    'click input.add': function (event) {
+      var name = $(event.currentTarget).prev().val();
+      if (name && name.length > 4)
+        Players.insert({name: name, score: randomScore()});
+    },
+    'click .player .delete': function () {
+      Players.remove(this._id);
+    }
+  };
+
+  // sorting
+
+  Template.header.events = {
+    'click #header .name': function () {
+      Session.set("name", Session.get("name") * (-1));
+    },
+    'click #header .score': function () {
+      Session.set("score", Session.get("score") * (-1));
     }
   };
 
@@ -30,22 +64,36 @@ if (Meteor.is_client) {
   };
 }
 
-// On server startup, create some players if the database is empty.
 if (Meteor.is_server) {
   var require = __meteor_bootstrap__.require,
     colors = require('colors');
 
+  // On server startup, create some players if the database is empty.
   Meteor.startup(function () {
-    console.log("Running Leaderborad on: http://localhost:3000/".rainbow)
+    console.log("Running Leaderborad on: http://0.0.0.0:3000/".rainbow)
+
     if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Math.random()*10)*5});
-    }
+      console.log('Initializing Players collection...');
+      [
+        "Ada Lovelace",
+        "Grace Hopper",
+        "Marie Curie",
+        "Carl Friedrich Gauss",
+        "Nikola Tesla",
+        "Claude Shannon"
+      ].forEach(function(name) {
+          Players.insert({ name: name });
+      });
+
+      Players.find().forEach(function(player) {
+        Players.update(player._id, {$set: {score: randomScore()}});
+      });
+
+    };
+
   });
+}
+
+function randomScore() {
+  return Math.floor(Math.random()*10)*5;
 }
